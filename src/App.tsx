2cf17ade56a9597da76b52918c8b1f22fa86994b
Aimen Altaiyeb <ymnghost@gmail.com>
2024-02-24 20:06:10 -0800

    Remove logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import styled from "styled-components";
 import './App.css';
 import searchBooks from "./services/OpenLibraryService";
