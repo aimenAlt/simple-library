c3cd349f27b9fff6095c8b170d30829c59b0dcae
aimenAlt <44252115+aimenAlt@users.noreply.github.com>
2024-02-25 04:26:35 +0000

    Deploying to gh-pages from @ aimenAlt/simple-library@f5bbf9166c749759db2223bfdd289e3f7dc719b6 🚀

--- src/interfaces/book.ts
+++
@@ -1,7 +0,0 @@
-export default interface Book {
-  title: string; //"title"
-  authors: string[]; //"author_name"
-  firstPublishedYear: number; //"first_publish_year"
-  isbn: string[]; // "isbn"
-  numberOfPages: number; //"number_of_pages_median"
-}
